c187ec3d96a7eaabb26cbaae839dcecacb36adab
Sara Pettigrew <sarajp.ak@hotmail.com>
2021-01-12 20:54:36 -0700

    Notes and code added to demo

--- src/typo.py
+++ src/typo.py
@@ -24,6 +24,28 @@ containsTypo(3) -> False
 containsTypo(5) -> True
 containsTypo(4) -> True
 """
+
+n = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
 def firstDraftWithTypo(n):
     # Your code here
+    pass
+    contains_typo = 4
+    min = 0
+    max = len(n) - 1
+    while not max < min:
+        guess = (min + max) // 2
+        # if containsTypo were real this line would be:
+        # if containsTypo(n[guess]) and not containsTypo(n[guess -1]):
+        if n[guess] == contains_typo and n[guess - 1] != contains_typo:
+            # resulting in the first occurrence where the typo exists
+            return n[guess]
+        # if containsTypo were real this line would be:
+        # if not containsTypo(n[guess]):
+        elif n[guess] < contains_typo:
+            min = guess + 1
+        else:
+            max = guess - 1
+    return -1
+# print(firstDraftWithTypo(n))
 
